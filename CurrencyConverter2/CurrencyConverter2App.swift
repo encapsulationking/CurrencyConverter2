19f6a0c6618f17dbacf56b1ae0e1ab2a38742685
joaobug <joaoenfrance@gmail.com>
2024-02-21 09:07:30 -0300

    Initial Commit

---
+++ CurrencyConverter2/CurrencyConverter2App.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverter2App.swift
+//  CurrencyConverter2
+//
+//  Created by Joao Leal on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverter2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
